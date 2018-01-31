5bf6b9ef9e2155bd82ba9918174d5dc02511df55
Stephanie Spears <Stephanie.Spears1@gmail.com>
2018-01-31 10:12:32 -0800

    add config

--- config.py
+++ config.py
@@ -10,3 +10,7 @@ class Config(object):
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
+    MS_TRANSLATOR_KEY = os.environ.get('MS_TRANSLATOR_KEY') or '02d36d7fdfa549bcb3884dbe2d5eb87f'
+    ELASTICSEARCH_URL = os.environ.get('ELASTICSEARCH_URL') or 'http://localhost:9200/'
+    GOOGLE_API_KEY = os.environ.get('GOOGLE_API_KEY') or 'AIzaSyBAKH4IaBUcaJdJf6xFWKhfoSKpO5O26rY'
+    HEROKU_DATABASE_URL = os.environ.get('HEROKU_DATABASE_URL') or 'postgres://jwuvdtxomudpcg:22dc713d15b9ee4645c2dda59de3c06ec72cf28468319a7b8046d0f88fbbe9e1@ec2-23-23-92-179.compute-1.amazonaws.com:5432/d6cl71kft21b6t'
