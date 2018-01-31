831cb05e8880c87b626a3cc9543662f1f303ba13
Stephanie Spears <Stephanie.Spears1@gmail.com>
2018-01-31 09:57:35 -0800

    update config var

--- config.py
+++ config.py
@@ -1,5 +1,11 @@
 import os
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config(object):
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'igotta2018'
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'f8jqlbmv6tc5utmr'
+    FLASK_APP = os.environ.get('FLASK_APP') or 'igotta.py'
+
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
