68cd53bda811afb8e5c8b50931835cf3ecda0a1d
Stephanie Spears <Stephanie.Spears1@gmail.com>
2018-02-04 09:45:20 -0800

    add posts to database

--- iGottaPackage/__init__.py
+++ iGottaPackage/__init__.py
@@ -1,10 +1,14 @@
 from flask import Flask
 from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 
 app = Flask(__name__)
 app.config.from_object(Config)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 
-from iGottaPackage import routes
+from iGottaPackage import routes, models
 
---
+++ iGottaPackage/models.py
@@ -0,0 +1,30 @@
+from datetime import datetime
+from iGottaPackage import db
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), index=True, unique=True)
+    email = db.Column(db.String(120), index=True, unique=True)
+    password_hash = db.Column(db.String(128))
+    posts = db.relationship('Post', backref='author', lazy='dynamic')
+    # The backref argument defines the name of a field that will be added to the objects of the "many" class that points back at the "one" object. This will add a post.author expression that will return the user given a post.
+
+    def __repr__(self):
+        return '<User {}>'.format(self.username)
+
+
+# TODO: add rating average, maybe comments
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(120))
+    body = db.Column(db.String(140))
+    photo = db.Column(db.String(240), nullable=True)
+    address = db.Column(db.String(240), unique=True)
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    # Passing datetime.utcnow function as a default, so SQLAlchemy will set the field to the value of calling that function (note that I did not include the () after utcnow, so the function itself, and not the result of calling it).
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    # 'user' is the db table, which Flask-SQLAlchemy automatically sets to the name of the model class converted to lowercase.
+
+    def __repr__(self):
+        return '<Post {}>'.format(self.body)
