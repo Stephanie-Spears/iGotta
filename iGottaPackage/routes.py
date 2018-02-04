3c3c6e87dc520eed4458029907b3e0cc2a08a781
Stephanie Spears <Stephanie.Spears1@gmail.com>
2018-02-04 14:33:29 -0800

    nothing

--- iGottaPackage/routes.py
+++ iGottaPackage/routes.py
@@ -55,7 +55,6 @@ def index():
         }
     ]
     return render_template('index.html', title='Home', posts=posts)
-
 # render_template is a function that comes with the Flask framework. This function takes a template filename and a variable list of template arguments and returns the same template, but with all the placeholders in it replaced with actual values. This also invokes the Jinja2 template engine that comes bundled with the Flask framework.
 # A python decorator (@) modifies the function that follows it. Usually they're used to register functions as callbacks for certain events. In this case, the @app.route decorator creates an association between the URL given as an argument and the function. In this example there are two decorators, which associate the URLs '/' and '/index' to this function. This means that when a web browser requests either of these two URLs, Flask is going to invoke this function and pass the return value of it back to the browser as a response.
 
