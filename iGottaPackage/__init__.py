4c7c0d7ffc18f9e761c2afd6e087d62dab86b0d3
Stephanie Spears <Stephanie.Spears1@gmail.com>
2018-02-07 02:27:49 -0800

    update procfile

--- iGottaPackage/__init__.py
+++ iGottaPackage/__init__.py
@@ -4,6 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_googlemaps import GoogleMaps
+from flask_bootstrap import Bootstrap
 from sqlalchemy_imageattach.stores.fs import HttpExposedFileSystemStore
 
 
@@ -14,6 +15,7 @@ migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
 GoogleMaps(app)
+bootstrap = Bootstrap(app)
 
 # Filesystem-backed storage implementation with WSGI middleware which serves actual image files.
 # fs_store = HttpExposedFileSystemStore('userimages', 'images/')
